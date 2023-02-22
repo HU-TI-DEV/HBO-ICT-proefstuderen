5df42d31609437b944462d18765e7ec177b96555
Hagen Patzke <hagen-git@hpatzke.nl>
2023-02-22 10:23:36 +0100

    cleanup, break up into more functions, make it Arduino-like

--- main.py
+++ main.py
@@ -5,39 +5,47 @@
 from machine import Pin
 from time import sleep_ms
 
+# Hardware definitions (which pin has which function)
 button = Pin(16, Pin.IN, Pin.PULL_DOWN)
 led_red = Pin(15, Pin.OUT)
 led_orange = Pin(14, Pin.OUT)
 led_green = Pin(13, Pin.OUT)
 buzzer = Pin(12, Pin.OUT)
 
-global button_pressed
-button_pressed = False
 
+# Function definitions ------------------------------------
 
-def beep_ten_times():
-    for i in range(10):
+def beep_n_times(number):
+    """ Beep ten times, on/off time is 200 milliseconds. """
+    for i in range(number):
         buzzer.value(1)
         sleep_ms(200)
         buzzer.value(0)
         sleep_ms(200)
+    return
 
 
-# initialisation, turn buzzer off
-buzzer.value(0)
-# self test, flash all LEDs three times
-# range(0) means i loops 0, 1, 2, 3, 4, 5, 6
-# (i & 1) == 0 if i is divisible by two
-for i in range(7):
-    led_red.value(i & 1)
-    led_green.value(i & 1)
-    led_orange.value(i & 1)
-    sleep_ms(200)
+def setup():
+    """ Setup function. Initialize hardware, run a small self test. """
+    # beep one time, ensures the beeper is off afterwards
+    beep_n_times(1)
+    # flash all LEDs three times
+    # range(7) means the variable i loops through 0, 1, 2, 3, 4, 5, 6
+    # (i & 1) == 0 if i is divisible by two, so the LED is on when i==1,3,5 and off when i==0,2,4,6
+    # By counting up to an even number we make sure all LEDs are turned off at the end.
+    for i in range(7):
+        led_red.value(i & 1)
+        led_green.value(i & 1)
+        led_orange.value(i & 1)
+        sleep_ms(200)
+    return
 
-while True:
-    if button.value() == 1:
+
+def loop():
+    """ This is the main execution loop. """
+    if button.value() == 1:  # if button is pressed
         led_red.value(1)
-        beep_ten_times()
+        beep_n_times(10)  # beep ten times
         sleep_ms(5000)
         led_orange.value(1)
         sleep_ms(2000)
@@ -49,5 +57,14 @@ while True:
         led_orange.value(1)
         sleep_ms(5000)
         led_orange.value(0)
+    # else: (otherwise) we do nothing
+    return
+
+
+# main program --------------------------------------------
+# With setup() and loop() our program structure is similar to an Arduino sketch.
+setup()
+while True:
+    loop()
 
 # eof
